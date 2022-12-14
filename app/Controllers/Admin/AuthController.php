469d07ea79c67b74b406903baedca10f81076f13
Dadan Hidayat <80499441+dadan-hidyt@users.noreply.github.com>
2022-12-14 14:10:48 +0700

    login method
    Login method

--- app/Controllers/Admin/AuthController.php
+++ app/Controllers/Admin/AuthController.php
@@ -7,7 +7,6 @@ namespace App\Controllers\Admin;
 use App\Controllers\BaseController;
 class AuthController extends BaseController{
     public function login(){
-        
         return view("",compact('admin'));
     }
 }
