f21d385858e7e19ed6b4fccf02a61b524cdcfa34
Dadan Hidayat <80499441+dadan-hidyt@users.noreply.github.com>
2022-12-14 15:40:36 +0700

    menambahkan koneksi ke database

---
+++ app/Database/Migrations/2022-12-14-081106_Users.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Users extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField(
+            array(
+                'id' => array(
+                    'type' => 'INT',
+                    'unsigned' => true,
+                    'auto_increment' => true,
+                ),
+                'nama_lengkap' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => '255',
+                ),
+                'email' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => '255',                
+                ),
+                'password' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => '255',                
+                ),
+                'level' => array(
+                    'type' => 'ENUM',
+                    'constraint' => ['super_admin','admin','user'],
+                    'default' => 'admin',
+                ),
+                'create_at' => array(
+                    'type' => 'datetime',
+                )
+        ));
+        $this->forge->addUniqueKey('email');
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable("tb_users", TRUE);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tb_users');
+    }
+}
