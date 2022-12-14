7e7b8a185ea9c1b2b4e85e154f8aa946f4316e59
dadan-hidyt <dadanhidyt@gmail.com>
2022-12-13 22:49:36 -0800

    routing dan production setting

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -21,26 +21,15 @@ $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
-// The Auto Routing (Legacy) is very dangerous. It is easy to create vulnerable apps
-// where controller filters or CSRF protection are bypassed.
-// If you don't want to define all routes, please use the Auto Routing (Improved).
-// Set `$autoRoutesImproved` to true in `app/Config/Feature.php` and set the following to true.
-// $routes->setAutoRoute(false);
-
-/*
- * --------------------------------------------------------------------
- * Route Definitions
- * --------------------------------------------------------------------
- */
-
-// We get a performance increase by specifying the default
-// route since we don't have to scan directories.
+//ROUTES
 $routes->get('/', 'Home::index');
-/**
-* Router FOR ADMIN PAGE
-*/
+//router group untuk admin
+$routes->group('admin',static function ($routes) {
+    //router login admin
+    $routes->get('login', "Admin\AuthController::login");
+    
+});
 
-$routes->get('wbdmin/login','WebAdmin/Auth::login');
 
 /*
  * --------------------------------------------------------------------
