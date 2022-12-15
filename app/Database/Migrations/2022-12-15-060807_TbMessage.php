7e029e11bbc1c61c2149168c4cd42ae790e4e801
Gigachard <120550492+Gigachard@users.noreply.github.com>
2022-12-15 13:26:11 +0700

    aa
    hai aku manusia

---
+++ app/Database/Migrations/2022-12-15-060807_TbMessage.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+use function PHPSTORM_META\type;
+
+class TbMessage extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField(
+            array(
+                'id' => array(
+                    'type' => 'INT',
+                    'auto_icrement' => true,
+                    'constraint' => '14',
+                    'null' => false,
+                ),
+                'nama' => array(
+                    'type' => 'VARCHAR',
+                    'constrait' => '225',
+                ),
+                'email' => array(
+                    'type' => 'VARCHAR',
+                    'constrait' => '225',
+                ),
+                'message' => array(
+                    'type' => 'TEXT',
+                    'null' => true,
+                )
+                )
+                );
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('tb_message');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tb_message');
+    }
+}
