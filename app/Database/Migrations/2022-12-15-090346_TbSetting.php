6163501dd9977e1e1cf90302fa0deec2d819c48b
Gigachard <120550492+Gigachard@users.noreply.github.com>
2022-12-15 16:09:52 +0700

    udahanpulang

---
+++ app/Database/Migrations/2022-12-15-090346_TbSetting.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TbSetting extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField(
+            array(
+                'id' => array(
+                    'type' => 'INT',
+                    'auto_increment' => true,
+                    'constraint' => '14',
+                    'null' => false,
+                ),
+                'favicon' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => '225',
+                ),
+                'judul' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => '225',
+                ),
+                'kata_kunci'
+            )
+        )
+    }
+
+    public function down()
+    {
+        //
+    }
+}
