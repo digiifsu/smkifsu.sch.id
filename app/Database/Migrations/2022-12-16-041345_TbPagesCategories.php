e852da7a7c589dcf0c579668145028d1fdbb6f23
GwenRossyV <gwenrossivan@gmail.com>
2022-12-16 12:55:54 +0700

    update 1

---
+++ app/Database/Migrations/2022-12-16-041345_TbPagesCategories.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TbPagesCategories extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField(
+            array(
+                'id' => array(
+                    'type' => 'INT',
+                    'auto_increment' => true,
+                    'constraint' => '14',
+                    'null' => false,
+                ),
+                'slug' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => '255',
+                ),
+                'nama' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => '250',
+                )
+            )
+        );
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('tb_PagesCategories');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tb_PagesCategories');
+    }
+}
