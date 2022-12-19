614118047a7acd855f919fb62100733e520aa55e
Dadan Hidayat <80499441+dadan-hidyt@users.noreply.github.com>
2022-12-19 10:08:46 +0700

    new a
    dada

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/smkifsu.sch.id/';
+    public $baseURL = '';
 
     /**
      * --------------------------------------------------------------------------
