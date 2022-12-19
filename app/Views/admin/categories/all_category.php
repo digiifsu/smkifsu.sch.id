bc7b6c055232de01f0cb958a0d5a6e0e949b8892
GwenRossyV <gwenrossivan@gmail.com>
2022-12-19 10:56:41 +0700

    edit add category

---
+++ app/Views/admin/categories/all_category.php
@@ -0,0 +1,6 @@
+<?php $this->extend('admin/layout/app_layout');?>
+
+<?php $this->section('content') ?>
+<h1>All Category</h1>
+
+<?php $this->endSection(); 
---
+++ app/Views/admin/categories/edit_category.php
@@ -0,0 +1,6 @@
+<?php $this->extend('admin/layout/app_layout') ?>
+
+<?php $this->section('content') ?>
+<h1>Edit Categories</h1>
+
+<?php $this->endSection(); ?>
